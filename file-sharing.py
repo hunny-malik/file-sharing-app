fa117327bcba0b2f8306300d655fbdbfb9ca3ce5
hunny malik <hunny.malik200524@gmail.com>
2024-10-08 22:21:42 +0530

    sharing done but with hostname

--- file-sharing.py
+++ file-sharing.py
@@ -11,6 +11,106 @@ root.geometry('450x560+500+200')
 root.configure(bg='#f4fdfe')
 root.resizable(False, False)
 
+def Send():
+    window = Toplevel(root)
+    window.title('Send')
+    window.geometry('450x560+500+200')
+    window.configure(bg = '#f4fdfe')
+    window.resizable(False, False)
+
+
+    def select_file():
+        global filename
+        filename = filedialog.askopenfilename(initialdir = os.getcwd(),
+                                                title = 'Select File',
+                                                filetype = (('file_type (*.txt)', '*.txt'),('all files (*.*)', '*.*')))
+
+
+    def sender():
+        s = socket.socket()
+        host = socket.gethostname()
+        port = 8080
+        s.bind((host,port))
+        s.listen(1)
+        print(host)
+        conn, addr = s.accept()
+        file = open(filename, 'rb')
+        file_data = file.read(1024)
+        conn.send(file_data)
+
+
+
+
+    image_icon1 = PhotoImage(file = 'Images/send.png')
+    window.iconphoto(False, image_icon1)
+
+    Sbackground = PhotoImage(file = 'Images/sender.png')
+    Label(window, image = Sbackground).place(x = -2, y = 0)
+
+    Mbackground = PhotoImage(file = 'Images/id.png')
+    Label(window, image = Mbackground, bg = '#f4fdfe').place(x = 100, y = 260)
+
+    host = socket.gethostname()
+    Label(window, text = f'ID: {host}', bg = 'white', fg = 'black', font = 'arial 15').place(x = 140, y = 290)
+
+
+    Button(window, text = '+ Select File', width = 10, height = 1, font = 'arial 14 bold', bg = '#fff', fg = '#000', command = select_file).place(x = 160, y = 150)
+    Button(window, text = 'Send', width = 8, height = 1, font = 'arial 14 bold', bg = '#000', fg = '#fff', command = sender).place(x = 300, y = 150)
+
+    window.mainloop()
+
+
+def Receive():
+    main = Toplevel(root)
+    main.title('Receive')
+    main.geometry('450x560+500+200')
+    main.configure(bg = '#f4fdfe')
+    main.resizable(False, False)
+
+
+    def receiver():
+        ID = SenderID.get()
+        filename1 = incoming_file.get()
+
+        s = socket.socket()
+        port = 8080
+        s.connect((ID, port))
+        file = open(filename1, 'wb')
+        file_data = s.recv(1024)
+        file.write(file_data)
+        file.close()
+
+
+    image_icon2 = PhotoImage(file = 'Images/receive.png')
+    main.iconphoto(False, image_icon2)
+
+    Hbackground = PhotoImage(file = 'Images/reciver.png')
+    Label(main, image = Hbackground).place(x = -2, y = 0)
+
+    logo = PhotoImage(file = 'Images/logo.png')
+    Label(main, image = logo, bg = '#f4fdfe').place(x =10, y = 250)
+
+    Label(main, text = 'Receive', font = ('arial', 20), bg = '#f4fdfe').place(x = 100, y = 280)
+
+    Label(main, text = 'Input sender ID', font = ('arial 10 bold'), bg = '#f4fdfe').place(x = 20, y = 340)
+    SenderID = Entry(main, width = 25, fg = 'black', border = 2, bg = 'white', font = ('arial', 15))
+    SenderID.place(x = 20, y = 370)
+    SenderID.focus()
+
+    Label(main, text = 'Filename for the incoming file:', font = ('arial 10 bold'), bg = '#f4fdfe').place(x = 20, y = 420)
+    incoming_file = Entry(main, width = 25, fg = 'black', border = 2, bg = 'white', font = ('arial', 15))
+    incoming_file.place(x = 20, y = 450)
+
+    rr = Button(main, text = 'Receive', compound = LEFT, width = 13, bg = '#39c790', font = 'arial 14 bold', command = receiver)
+    rr.place(x = 20, y = 500)
+
+
+
+
+    main.mainloop()
+
+
+
 
 
 #icon code
@@ -23,22 +123,22 @@ Label(root, text = 'File Sharing', font = ('SangBleu', 20, 'bold'), bg = '#f4fdf
 Frame(root, width = 400, height = 2, bg = '#f3f5f6').place(x = 25, y = 80)
 
 send_image = PhotoImage(file = 'Images/send.png')
-send = Button(root, image = send_image, bg = '#f4fdfe', bd = 0, width=100, height=50)
-send.place(x = 50, y = 100)
+send = Button(root, image = send_image, bg = '#f4fdfe', bd = 0, width=100, height=100, command = Send)
+send.place(x = 50, y = 85)
 
 receive_image = PhotoImage(file = 'Images/receive.png')
-receive = Button(root, image = receive_image, bg = '#f4fdfe', bd = 0, width=100, height=50)
-receive.place(x = 300, y = 100)
+receive = Button(root, image = receive_image, bg = '#f4fdfe', bd = 0, width=100, height=100, command = Receive)
+receive.place(x = 300, y = 85)
 
 
-Label(root, text = 'Send', font = ('Acumin Variable concept', 17, 'bold'), bg = '#f4fdfe').place(x = 65, y = 150)
-Label(root, text = 'Receive', font = ('Acumin Variable concept', 17, 'bold'), bg = '#f4fdfe').place(x = 305, y = 150)
+Label(root, text = 'Send', font = ('Acumin Variable concept', 17, 'bold'), bg = '#f4fdfe').place(x = 65, y = 165)
+Label(root, text = 'Receive', font = ('Acumin Variable concept', 17, 'bold'), bg = '#f4fdfe').place(x = 305, y = 165)
 
 
 background = PhotoImage(file = 'Images/background.png')
-Label(root, image = background).place(x = -2, y = 334)
+Label(root, image = background).place(x = -2, y = 335)
 
-Label(root, text = 'Team Name', font = ('SangBleu', 30, 'bold'), bg = '#f4fdfe').place(x = 25, y = 265)
+Label(root, text = 'Team Name', font = ('SangBleu', 30, 'bold'), bg = '#f4fdfe').place(x = 25, y = 266)
 
 
 
